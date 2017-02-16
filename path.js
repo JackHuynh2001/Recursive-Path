d1fd0721e144d9db8eb8abb709af4f0bf65c3290
JackHuynh2001 <9jhuynh@stpatrick.org>
2017-02-16 13:57:56 -0600

    Add files via upload

---
+++ path.js
@@ -0,0 +1,48 @@
+//Your code below
+function buildpath(p,n){
+  let path=[];
+  for(let a=0;a<n;a++){
+  }
+  let r = Math.Random
+  if(r<p){
+    let path = false
+  }
+  else if (r>=p){
+    let path = true
+  }
+function stroll(path,index){
+  if(index=path.length-1){
+    return 1;
+
+  }
+else if(path[index]==false){
+  return 0;
+
+}
+}
+else{
+  return stroll(path,index+1);
+}
+
+}
+function rsj(path,index){
+  if(index==path.length-1)
+  return 1;
+}
+  else if(path[index]==false){
+    resturn 0;
+  }
+else
+  return rsj(path,index+1)+rsj(path,index+2)+rsj(path,index+5);
+}
+}
+//Test code
+let testpath1 = buildPath(0.10,15);
+let testpath2 = buildPath(0.50,15);
+console.log(testpath1);
+console.log(testpath2);
+console.log("---------------------");
+console.log(stroll([true,false,true],0));
+console.log(stroll([true,true,true],0));
+console.log(rsj(testpath1,0));
+console.log(rsj(testpath2,0));
